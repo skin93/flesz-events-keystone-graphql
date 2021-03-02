550f025ca2ea3b6cfb01da4a0b5a863421ef1314
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-02 22:04:29 +0100

    Setup Apollo client, check random query

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+export const ALL_POSTS_QUERY = gql`
+  query {
+    allPosts {
+      title
+      slug
+      category {
+        name
+      }
+      createdAt
+    }
+  }
+`
 
 const HomePage = () => {
-  return <div>Hello Page</div>
+  const { loading, error, data } = useQuery(ALL_POSTS_QUERY)
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Something went wrong</div>
+  }
+  return <div>{JSON.stringify(data)}</div>
 }
 
 export default HomePage
