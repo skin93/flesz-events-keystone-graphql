eba5c741a4272b818bccc91bee2011e77dc172f7
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-03 16:39:40 +0100

    Retrieve necessary fields with SinglePostQuery

--- client/lib/queries/posts/singlePostQuery.js
+++ client/lib/queries/posts/singlePostQuery.js
@@ -2,7 +2,20 @@ import { gql } from '@apollo/client'
 export const SINGLE_POST_QUERY = gql`
   query SinglePostQuery($slug: String!) {
     allPosts(where: { slug: $slug }) {
-      slug
+      title
+      excerpt
+      body
+      cover_url
+      category {
+        name
+      }
+      tags {
+        name
+      }
+      createdAt
+      authors {
+        name
+      }
     }
   }
 `
