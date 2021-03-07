7ac3bbfedc0a78d5578cacb1d27bd4b018f73799
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-07 16:44:16 +0100

    Modify the BaseCard compoennt

--- client/components/UI/BaseCard/index.js
+++ client/components/UI/BaseCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
@@ -6,70 +7,6 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Chip from '@material-ui/core/Chip'
 import Typography from '@material-ui/core/Typography'
-import Hidden from '@material-ui/core/Hidden'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: 600,
-    height: 300,
-    [theme.breakpoints.down('md')]: {
-      height: 400
-    },
-    position: `relative`
-  },
-  actionsArea: {
-    height: `100%`
-  },
-  chip: {
-    position: `absolute`,
-    top: 0,
-    left: 0,
-    fontWeight: `bold`,
-    textTransform: `uppercase`,
-    backgroundColor: theme.palette.accent.main,
-    color: `black`,
-    borderRadius: `5px 0px 0px 0px`
-  },
-  media: {
-    height: '100%',
-    [theme.breakpoints.down('sm')]: {
-      height: 200
-    }
-  },
-  bodyDesktop: {
-    display: `flex`,
-    flexDirection: `column`,
-    justifyContent: `center`,
-    position: `absolute`,
-    bottom: 0,
-    left: 0,
-    background: `black`,
-    width: `100%`,
-    height: 100,
-    opacity: 0.8
-  },
-  bodyMobile: {
-    display: `flex`,
-    flexDirection: `column`,
-    justifyContent: `center`,
-    backgroundColor: theme.palette.background.main,
-    width: `100%`,
-    height: 200,
-    opacity: 0.8
-  },
-  title: {
-    fontWeight: `bold`,
-    color: theme.palette.light.main,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '1.2rem'
-    }
-  },
-  created: {
-    color: theme.palette.accent.main,
-    fontWeight: `bold`
-  }
-}))
 
 const BaseCard = ({ post }) => {
   const classes = useStyles()
@@ -83,48 +20,81 @@ const BaseCard = ({ post }) => {
           title={post.title}
           alt={post.title}
         />
-        <Chip label={post.category.name} className={classes.chip} />
-        <Hidden smDown>
-          <CardContent className={classes.bodyDesktop}>
-            <Typography
-              gutterBottom
-              variant='subtitle1'
-              component='h2'
-              className={classes.title}
-            >
-              {post.title}
-            </Typography>
-            <Typography
-              variant='subtitle2'
-              component='p'
-              className={classes.created}
-            >
-              {post.createdAt.split('T')[0]}
-            </Typography>
-          </CardContent>
-        </Hidden>
-        <Hidden mdUp>
-          <CardContent className={classes.bodyMobile}>
-            <Typography
-              gutterBottom
-              variant='subtitle1'
-              component='h2'
-              className={classes.title}
-            >
-              {post.title}
-            </Typography>
-            <Typography
-              variant='subtitle2'
-              component='p'
-              className={classes.created}
-            >
-              {post.createdAt.split('T')[0]}
-            </Typography>
-          </CardContent>
-        </Hidden>
+        <Link href={`/categories/${post.category.slug}`}>
+          <a>
+            <Chip label={post.category.name} className={classes.chip} />
+          </a>
+        </Link>
+        <CardContent className={classes.content}>
+          <Typography
+            gutterBottom
+            variant='subtitle1'
+            component='h2'
+            className={classes.title}
+          >
+            {post.title}
+          </Typography>
+          <Typography
+            variant='subtitle2'
+            component='p'
+            className={classes.created}
+          >
+            {post.createdAt.split('T')[0]}
+          </Typography>
+        </CardContent>
       </CardActionArea>
     </Card>
   )
 }
 
 export default BaseCard
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: 'auto',
+    position: 'relative',
+    borderRadius: '10px'
+  },
+  actionsArea: {
+    height: '100%'
+  },
+  chip: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: '.8em',
+    backgroundColor: theme.palette.accent.main,
+    color: theme.palette.black.main,
+    borderRadius: '10px 0px 0px 0px',
+    cursor: 'pointer'
+  },
+  media: {
+    height: 150,
+    [theme.breakpoints.down('sm')]: {
+      height: 200
+    }
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: theme.palette.background.main,
+    width: '100%',
+    height: '100%',
+    opacity: 0.8
+  },
+  title: {
+    fontWeight: 'bold',
+    color: theme.palette.light.main,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.2rem'
+    }
+  },
+  created: {
+    color: theme.palette.muted.main,
+    fontWeight: 'bold'
+  }
+}))
