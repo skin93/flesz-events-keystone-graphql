060471c8f75402ba901be76dc8bd2546fb1e3362
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-07 18:25:42 +0100

    Make a iframe responsive

--- client/theme.js
+++ client/theme.js
@@ -5,6 +5,21 @@ const theme = createMuiTheme({
   overrides: {
     MuiCssBaseline: {
       '@global': {
+        '.iframe-container': {
+          position: 'relative',
+          overflow: 'hidden',
+          width: '100%',
+          paddingTop: '56.25%',
+
+          '& iframe': {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            width: '100%',
+            height: '100%'
+          }
+        },
         body: {
           padding: 0,
           margin: 0,
@@ -16,18 +31,20 @@ const theme = createMuiTheme({
           padding: `30px`,
           lineHeight: `2em`,
           margin: `30px auto`,
-          width: `80%`,
+          width: `100%`,
           borderLeft: `5px solid #32e0c4`,
           fontSize: `1.2rem`
         },
-        iframe: {
-          position: `relative`,
-          left: `50%`,
-          transform: `translateX(-50%)`,
-          width: `100%`,
-          maxHeight: `100%`,
-          margin: `30px 0`
-        },
+        // iframe: {
+        //   position: `relative`,
+        //   left: `50%`,
+        //   transform: `translateX(-50%)`,
+        //   width: '90%',
+        //   maxWidth: `560px`,
+        //   maxHeight: '315px',
+        //   height: '315px',
+        //   margin: `30px 0`
+        // },
         h2: {
           color: `#eee`,
           fontSize: `2rem`
