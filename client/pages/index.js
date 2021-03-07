f960c84a9de14313e95996c27355fdc6eb28dbab
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-07 21:40:17 +0100

    Add load more button

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,6 +6,7 @@ import { ALL_POSTS_QUERY } from '../lib/queries/posts/allPostsQuery'
 
 import Error from '../components/Error'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 import Fade from '@material-ui/core/Fade'
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -14,21 +15,29 @@ import BaseCard from '../components/UI/BaseCard'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 const HomePage = () => {
-  const { loading, error, data } = useQuery(ALL_POSTS_QUERY)
+  const [skip, setSkip] = React.useState(0)
+  const [first, setFirst] = React.useState(1)
+  const { loading, error, data } = useQuery(ALL_POSTS_QUERY, {
+    variables: { skip, first }
+  })
   const classes = useStyles()
+
+  const handleClick = () => {
+    setFirst((prev) => prev + 1)
+  }
 
   if (loading) {
     return <div>Loading...</div>
   }
   if (error) {
-    return <Error message='Can not fetch data' />
+    return <Error message={error.errors[0].message} />
   }
   return (
     <main className={classes.root}>
       <Typography variant='h6' className={classes.heading}>
         OSTATNIE WPISY
       </Typography>
-      <Fade in='true' timeout={500}>
+      <Fade in={true} timeout={500}>
         <ResponsiveMasonry
           columnsCountBreakPoints={{ 0: 1, 600: 2, 960: 3 }}
           className={classes.container}
@@ -44,6 +53,14 @@ const HomePage = () => {
           </Masonry>
         </ResponsiveMasonry>
       </Fade>
+      <Button
+        onClick={handleClick}
+        variant='outlined'
+        className={classes.loadMoreButton}
+        disabled={data._allPostsMeta.count === data.allPosts.length}
+      >
+        Wczytaj więcej
+      </Button>
     </main>
   )
 }
@@ -58,5 +75,11 @@ const useStyles = makeStyles((theme) => ({
   },
   container: {
     marginTop: `30px`
+  },
+  loadMoreButton: {
+    display: 'block',
+    margin: '30px auto',
+    fontWeight: 'bold',
+    color: theme.palette.accent.main
   }
 }))
