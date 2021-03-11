cbb6c1ce94dde1fbec5625d44aa155e01f25f78d
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-11 16:26:56 +0100

    Add a preview page

---
+++ client/lib/queries/posts/previewPostQuery.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client'
+export const PREVIEW_POST_QUERY = gql`
+  query previewPostQuery($id: ID!) {
+    allPosts(where: { AND: [{ id: $id }, { status: DRAFT }] }) {
+      title
+      excerpt
+      body
+      cover_url
+      cover_src
+      category {
+        name
+        slug
+      }
+      tags {
+        name
+        slug
+      }
+      createdAt
+      authors {
+        name
+      }
+    }
+  }
+`
