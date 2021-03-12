fb855169098ce0c37e6472860be78f84fca67c60
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-03-12 22:48:56 +0100

    Set a custom server for production

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { keystone, apps } = require('./index.js')
+
+keystone
+  .prepare({
+    apps: apps,
+    dev: process.env.NODE_ENV !== 'production'
+  })
+  .then(async ({ middlewares }) => {
+    await keystone.connect()
+    const app = express()
+
+    app.use(middlewares).listen(3015)
+  })
